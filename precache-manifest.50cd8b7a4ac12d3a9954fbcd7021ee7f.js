2c8da42ddb60dd9fb3d24f8d9d958aa2df3a614e
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-07-14 17:27:16 +0200

    Updates

---
+++ precache-manifest.50cd8b7a4ac12d3a9954fbcd7021ee7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d4137efbe7877767474076bea7ef99",
+    "url": "/todolists-React/index.html"
+  },
+  {
+    "revision": "9828faa40645394f8428",
+    "url": "/todolists-React/static/css/main.a2cdb6d3.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todolists-React/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolists-React/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9828faa40645394f8428",
+    "url": "/todolists-React/static/js/main.93467db4.chunk.js"
+  },
+  {
+    "revision": "f6e19cf3f353d7291367",
+    "url": "/todolists-React/static/js/runtime-main.500b4c3f.js"
+  }
+]);
