e20a02ca7ed94b5ef6981a7bfebd02c36cd936f2
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-08-03 09:52:02 +0200

    adding net task interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-const Form = (props) => (
-    <form className="form">
-        <input className="form__inputElement" />
-        <button className="form__buttonElement">Dodaj zadanie</button>
-    </form>
-);
+const Form = ({ addNewTask }) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__inputElement"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__buttonElement">Dodaj zadanie</button>
+        </form>
+    );
+};
 
 export default Form;
