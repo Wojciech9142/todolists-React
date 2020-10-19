5f5bfb82d27e28bae51dfc6092b86a9f8a0d14d5
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-10-19 19:15:33 +0200

    fixed local storage

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,9 +1,8 @@
 export const getExampleTasks = async () => {
     const response = await fetch("/todolists-React/exampleTasks.json");
 
-    if(!response.ok){
+    if (!response.ok) {
         new Error(response.statusText);
     }
-
     return await response.json();
 };
--- src/features/tasks/taskLocalStorage.js
+++ src/features/tasks/taskLocalStorage.js
@@ -3,5 +3,10 @@ const localStorageKey = "tasks";
 export const saveTasksInLocalStorage = tasks => 
     localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-export const getTasksFromLocalStorage = () => 
+export const getTasksFromLocalStorage = () => {
+    if(typeof(localStorage.getItem(localStorageKey))=="undefined"){
+        return [];
+ }else{
     JSON.parse(localStorage.getItem(localStorageKey))
+ }
+}
